6a59ca526bd5d899d102ba5a909ee12975d8d537
tmarsh <ts.marsh@gmail.com>
2016-11-03 15:26:26 -0400

    more flexible exceptions

--- core/src/main/java/com/tailoredshapes/underbar/function/ExceptionalFunctions.java
+++ core/src/main/java/com/tailoredshapes/underbar/function/ExceptionalFunctions.java
@@ -3,15 +3,26 @@ package com.tailoredshapes.underbar.function;
 /**
  * Created by tmarsh on 11/3/16.
  */
-public class ExceptionalFunctions {
+public interface ExceptionalFunctions {
+
     @FunctionalInterface
     public interface RunnableWithOops<E extends Throwable> {
+        void run() throws Throwable;
+    }
+
+    public interface RunnableThatMight<E extends Throwable> {
         void run() throws E;
     }
 
     @FunctionalInterface
     public interface SupplierWithOops<T> {
         T get() throws Throwable;
+    }
+
+
+    @FunctionalInterface
+    public interface SupplierWThatMight<T, E extends Throwable> {
+        T get() throws E;
     }
 
 }
