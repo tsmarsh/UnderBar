8b6ead50d98a0162460a5d16cafe12a23a0c9531
tmarsh <ts.marsh@gmail.com>
2016-11-04 07:56:58 -0400

    clean up

--- core/src/main/java/com/tailoredshapes/underbar/function/RegularFunctions.java
+++ core/src/main/java/com/tailoredshapes/underbar/function/RegularFunctions.java
@@ -5,62 +5,62 @@ package com.tailoredshapes.underbar.function;
  */
 public interface RegularFunctions {
     @FunctionalInterface
-    public static interface SeptaConsumer<S , T, U, V, X, Y, Z> {
+    interface SeptaConsumer<S , T, U, V, X, Y, Z> {
         void accept(S s, T t, U u, V v, X x, Y y, Z z);
     }
 
     @FunctionalInterface
-    public static interface SeptaFunction<T, U, V, W, X, Y, Z, R> {
+    interface SeptaFunction<T, U, V, W, X, Y, Z, R> {
         R apply(T t, U u, V v, W w, X x, Y y, Z z);
     }
 
     @FunctionalInterface
-    public static interface TriConsumer<T, U, V> {
+    interface TriConsumer<T, U, V> {
         void accept(T t, U u, V v);
     }
 
     @FunctionalInterface
-    public static interface TriFunction<T, U, V, R> {
+    interface TriFunction<T, U, V, R> {
         R apply(T t, U u, V v);
     }
 
     @FunctionalInterface
-    public static interface QuadFunction<T, U, V, W, R> {
+    interface QuadFunction<T, U, V, W, R> {
         R apply(T t, U u, V v, W w);
     }
 
     @FunctionalInterface
-    public static interface HexFunction<T, U, V, W, X, Y, R> {
+    interface HexFunction<T, U, V, W, X, Y, R> {
         R apply(T t, U u, V v, W w, X x, Y y);
     }
 
     @FunctionalInterface
-    public static interface QuadConsumer<T, U, V, X> {
+    interface QuadConsumer<T, U, V, X> {
         void accept(T t, U u, V v, X x);
     }
 
     @FunctionalInterface
-    public static interface HexConsumer<T, U, V, X, Y, Z> {
+    interface HexConsumer<T, U, V, X, Y, Z> {
         void accept(T t, U u, V v, X x, Y y, Z z);
     }
 
     @FunctionalInterface
-    public static interface OctaConsumer<R, S , T, U, V, X, Y, Z> {
+    interface OctaConsumer<R, S , T, U, V, X, Y, Z> {
         void accept(R r, S s, T t, U u, V v, X x, Y y, Z z);
     }
 
     @FunctionalInterface
-    public static interface OctaFunction<T, U, V, W, X, Y, Z, A, R> {
+    interface OctaFunction<T, U, V, W, X, Y, Z, A, R> {
         R apply(T t, U u, V v, W w, X x, Y y, Z z, A a);
     }
 
     @FunctionalInterface
-    public static interface PentaFunction<T, U, V, W, X, R> {
+    interface PentaFunction<T, U, V, W, X, R> {
         R apply(T t, U u, V v, W w, X x);
     }
 
     @FunctionalInterface
-    public static interface PentaConsumer<T, U, V, X, Y> {
+    interface PentaConsumer<T, U, V, X, Y> {
         void accept(T t, U u, V v, X x, Y y);
     }
 }
