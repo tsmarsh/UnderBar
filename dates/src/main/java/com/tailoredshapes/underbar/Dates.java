5a423ec27218490fc0d38ca52f203814fac5f492
tmarsh <ts.marsh@gmail.com>
2016-11-04 07:44:23 -0400

    Better dates

--- dates/src/main/java/com/tailoredshapes/underbar/Dates.java
+++ dates/src/main/java/com/tailoredshapes/underbar/Dates.java
@@ -1,14 +1,11 @@
 package com.tailoredshapes.underbar;
 
 import java.time.Instant;
-import java.time.YearMonth;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 import java.util.Date;
 
-import static java.time.format.DateTimeFormatter.ISO_DATE;
 import static java.time.format.DateTimeFormatter.ISO_INSTANT;
 
 public class Dates {
@@ -17,24 +14,20 @@ public class Dates {
         return Date.from(instant);
     }
 
-    public static Date date(YearMonth yearMonth) {
-        return Date.from(yearMonth.atDay(1).atStartOfDay(ZoneOffset.UTC).toInstant());
-    }
-
     public static Date date(String string) {
         TemporalAccessor parse = DateTimeFormatter.ISO_INSTANT.withZone(ZoneOffset.UTC).parse(string);
         return date(Instant.from(parse));
     }
 
-    public static Instant instant(String string){
-        return Instant.from(ISO_DATE.parse(string));
+    public static Instant instant(String string) {
+        return Instant.from(ISO_INSTANT.parse(string));
     }
 
     public static String isoString(Date time) {
         return ISO_INSTANT.format(time.toInstant());
     }
 
-    public static String isoString(Instant instant){
+    public static String isoString(Instant instant) {
         return ISO_INSTANT.format(instant);
     }
 }
--- dates/src/test/java/com/tailoredshapes/underbar/DatesTest.java
+++ dates/src/test/java/com/tailoredshapes/underbar/DatesTest.java
@@ -11,6 +11,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import static com.tailoredshapes.underbar.Dates.date;
+import static com.tailoredshapes.underbar.Dates.instant;
 import static com.tailoredshapes.underbar.Dates.isoString;
 import static java.time.format.DateTimeFormatter.ISO_DATE;
 import static org.junit.Assert.assertEquals;
@@ -29,6 +30,15 @@ public class DatesTest {
         Date expected = Date.from(now.toInstant());
 
         assertEquals(expected, date(date));
+    }
+
+    @Test
+    public void instantReturnsANewInstant() throws Exception {
+        ZonedDateTime now = ZonedDateTime.now();
+
+        String date = now.format(DateTimeFormatter.ISO_INSTANT);
+
+        assertEquals(now.toInstant(), instant(date));
     }
 
     @Test
