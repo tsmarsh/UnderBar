e6b5bbbcf3c3c3dd26a997431d7aa4eb68c1f486
tmarsh <ts.marsh@gmail.com>
2017-01-26 20:33:17 -0500

    Always clean your imports

--- io/src/test/java/com/tailoredshapes/underbar/IOTest.java
+++ io/src/test/java/com/tailoredshapes/underbar/IOTest.java
@@ -1,16 +1,13 @@
 package com.tailoredshapes.underbar;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.junit.Test;
 
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.nio.charset.Charset;
 import java.util.Date;
 
 import static com.tailoredshapes.stash.Stash.stash;
-import static com.tailoredshapes.underbar.Dates.date;
 import static com.tailoredshapes.underbar.IO.*;
 import static org.junit.Assert.assertEquals;
 
