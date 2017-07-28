8ec25bfa15c17c749f9ec434cebb3d265bbb0dc6
Tom Marsh <ts.marsh@gmail.com>
2017-07-28 10:13:02 -0400

    Rename ready to start work on RSA

---
+++ crypto/src/test/java/com/tailoredshapes/underbar/crypto/AESTest.java
@@ -0,0 +1,44 @@
+package com.tailoredshapes.underbar.crypto;
+
+import org.junit.Test;
+
+import javax.crypto.SecretKey;
+
+import static com.tailoredshapes.underbar.crypto.AES.*;
+import static org.junit.Assert.*;
+
+public class AESTest {
+
+    @Test
+    public void shouldEncryptAndDecryptAMessage() throws Exception {
+        SecretKey key = aesKey();
+
+        assertEquals("My message", decrypt(key, encrypt(key, "My message".getBytes())).toString());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldNotDecryptWithWrongKey() throws Exception {
+        SecretKey key = aesKey();
+        SecretKey badKey = aesKey();
+
+        decrypt(badKey, encrypt(key, "My message".getBytes()));
+    }
+
+    @Test
+    public void shouldEncryptAndDecryptStrings() throws Exception {
+        SecretKey key = aesKey();
+
+        Payload payload = encrypt(key, "My message");
+        Payload payloadFromString = new Payload(payload.asString(), payload.iv);
+        assertEquals("My message", decrypt(key, payloadFromString).toString());
+    }
+
+    @Test
+    public void shouldEncryptAndDecryptURLSafeStrings() throws Exception {
+        SecretKey key = aesKey();
+        Payload encrypted = encrypt(key, "My message");
+        String urlSafeCipher = encrypted.asString(true);
+
+        assertEquals("My message", decrypt(key, new Payload(urlSafeCipher, encrypted.iv, true)).toString());
+    }
+}
